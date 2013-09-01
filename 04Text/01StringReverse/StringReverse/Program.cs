38fc5823d7e4cf04d9bcd9a01a3d5643e5fea847
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-09-01 11:32:45 +0200

    Add StringReverse

---
+++ 04Text/01StringReverse/StringReverse/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StringReverse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.WriteLine("StringReverse takes excatly 1 string argument.");
+                Environment.Exit(1);
+            }
+            char[] input_word = args[0].ToCharArray();
+            Array.Reverse(input_word);
+            Console.WriteLine(new string(input_word));
+        }
+    }
+}
