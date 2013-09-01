6769d420e1dc788c0a5962ae63a4c0efe61dde8e
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-09-01 14:51:26 +0200

    Add Caesar Cipher

---
+++ 12Security/01CaesarCipher/CaesarCipher/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input_string = "Hello WorldazAZ";
+            Code code = new Code(1);
+
+            Console.WriteLine("Input String: " + input_string);
+
+            input_string = input_string.ToUpper();
+
+            Console.WriteLine("Encoded: " + code.encode(input_string));
+            Console.WriteLine("Decoded again: " + code.decode(code.encode(input_string)));
+        }
+    }
+}
---
+++ 12Security/01CaesarCipher/CaesarCipher/code.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CaesarCipher
+{
+    class Code
+    {
+        private int LOWER_BOUND = (int)'A';
+        private int UPPER_BOUND = (int)'Z';
+        public int modifier;
+
+        public Code(int modifier)
+        {
+            this.modifier = modifier;
+        }
+
+        public string decode(string unencoded)
+        {
+            return code_helper(unencoded, modifier);
+        }
+
+        public string encode(string unencoded)
+        {
+            return code_helper(unencoded, -modifier);
+        }
+
+        private string code_helper(string unencoded, int modifier)
+        {
+            int ch_int, new_ch_int, i=0;
+            char[] result = new char[unencoded.Length];
+
+            foreach (char ch in unencoded)
+            {
+                ch_int = (int)ch;
+                if (ch_int < LOWER_BOUND || ch_int > UPPER_BOUND)
+                {
+                    result[i++] = (char)ch_int;
+                    continue;
+                }
+                new_ch_int = ch_int + modifier;
+                if (new_ch_int > UPPER_BOUND)
+                {
+                    new_ch_int = LOWER_BOUND + (new_ch_int - UPPER_BOUND - 1);
+                }
+                else if (new_ch_int < LOWER_BOUND)
+                {
+                    new_ch_int = UPPER_BOUND - (LOWER_BOUND - new_ch_int - 1);
+                }
+                result[i++] = (char)new_ch_int;
+            }
+
+            return new string(result);
+        }
+    }
+}
