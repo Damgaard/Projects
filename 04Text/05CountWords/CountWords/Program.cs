749146c9ce24cbfef618039edbf1ab492c23d9f8
Andreas Damgaard Pedersen <andreas.damgaard.pedersen@gmail.com>
2013-09-03 23:07:29 +0200

    Add Count Words

---
+++ 04Text/05CountWords/CountWords/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CountWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(countWords("Hello World"));
+        }
+
+        static int countWords(string input_string)
+        {
+            string pattern = @"\b\w+\b";
+            Regex rgx = new Regex(pattern);
+            return rgx.Matches(input_string).Count;
+        }
+    }
+}
